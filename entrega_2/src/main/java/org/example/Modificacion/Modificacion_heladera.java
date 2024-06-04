93a32fa27cd2c5223a223866c7b07e1d8e0c397d
JuanMarcosCorbalan <jcorbalan@frba.utn.edu.ar>
2024-06-04 19:06:13 -0300

    agrego modificacion de heladera

---
+++ entrega_2/src/main/java/org/example/Modificacion/Modificacion_heladera.java
@@ -0,0 +1,11 @@
+package org.example.Modificacion;
+
+import org.example.Heladeras.Heladera;
+import org.example.Heladeras.PuntoUbicacion;
+
+public class Modificacion_heladera {
+
+    public void modificar_ubicacion(Heladera heladera, PuntoUbicacion puntoUbicacion){
+        heladera.setPuntoUbicacion(puntoUbicacion);
+    }
+}
