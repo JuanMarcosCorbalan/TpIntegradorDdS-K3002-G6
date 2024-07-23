056026f07bc647b29d99965037179071b1d2ef6e
JoaquinBergara <jbergara@frba.utn.edu.ar>
2024-07-23 18:36:02 -0300

    test de registrar finalizados

--- entrega_2.5/src/main/java/org/example/Sistema/RegistrarUsuario.java
+++ entrega_2.5/src/main/java/org/example/Sistema/RegistrarUsuario.java
@@ -5,49 +5,57 @@ import org.example.ValidarContrasenia.ValidarContrasenia;
 import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 import java.util.Scanner;
 
 public class RegistrarUsuario {
 
     static Map<String,String> usuarios_contrasenias = new HashMap<String,String>();
     public static void main(String[] args) throws IOException {
+            Scanner scanner = new Scanner(System.in);
 
-        Scanner scanner = new Scanner(System.in);
+            System.out.print("Ingresar nombre de usuario:");
+            String nombre = scanner.nextLine();
+            System.out.print("Ingresar contraseña: ");
+            String contrasenia = scanner.nextLine();
 
-        System.out.print("Ingresar nombre de usuario:");
-        String nombre = scanner.nextLine();
-        System.out.print("Ingresar contraseña: ");
-        String contrasenia = scanner.nextLine();
-
-        comprobar_registrar_usuario(nombre,contrasenia);
-
-        //FALTA VALIDAR QUE NO HAYA DOS USUARIOS IGUALES, NO DEJAR REGISTRAR SI YA HAY UNO CON ESE NOMBRE
-        //HACER AMBAS VALIDACIONES EN UN SOLO METODO LLAMADO VALIDAR REGISTRO
-        //CAMBIARLE EL NOMBRE A LA FUNCION INTEGRADORA, ALGO MAS DECLARATIVO
+            comprobar_registrar_usuario(nombre, contrasenia);
 
 
     }
 
     public static void comprobar_registrar_usuario(String nombre,String contrasenia) throws IOException {
-        if(validar_usuario(nombre,contrasenia))
+        if(validar_registro(nombre,contrasenia))
         {
             System.out.print("\nUSUARIO REGISTRADO CON EXITO\n");
             registrar_usuario(nombre,contrasenia);
         }else
         {
-            System.out.print("\nCONTRASEÑA INVALIDA, USUARIO RECHAZADO\n");
+            System.out.print("\nUSUARIO RECHAZADO\n");
         }
     }
 
 
+    public static boolean validar_registro(String nombre, String contrasenia) throws IOException {
+        return validar_nombre(nombre) && validar_contrasenia(contrasenia);
+    }
 
     public static void registrar_usuario(String nombre, String contrasenia) {
         usuarios_contrasenias.put(nombre,contrasenia);
     }
 
-    public static boolean validar_usuario(String usuario, String contrasenia) throws IOException {
+    public static boolean validar_contrasenia(String contrasenia) throws IOException {
         ValidarContrasenia validarContrasenia = new ValidarContrasenia();
         return validarContrasenia.validar(contrasenia);
     }
-
+    public static boolean validar_nombre(String nombre) throws IOException {
+        for(String key : usuarios_contrasenias.keySet())
+        {
+            if(Objects.equals(nombre, key)) {
+                System.out.print("\n YA EXISTE UN USUARIO REGISTRADO CON ESE NOMBRE \n");
+                return false;
+            }
+        }
+        return true;
+    }
 }
--- entrega_2.5/src/test/java/org/example/Sistema/TestsRegistroUsuarios.java
+++ entrega_2.5/src/test/java/org/example/Sistema/TestsRegistroUsuarios.java
@@ -21,6 +21,15 @@ public class TestsRegistroUsuarios {
         boolean usuario_en_hash_map = RegistrarUsuario.usuarios_contrasenias.containsKey("arbolito23");
         assertFalse(usuario_en_hash_map,"Se rechaza el registro del usuario por contrasenia invalida");
     }
+
+    @Test
+    void usuario_existente() throws IOException {
+        RegistrarUsuario.comprobar_registrar_usuario("rood92","Hola_1234");
+        RegistrarUsuario.comprobar_registrar_usuario("rood92","Comova_12345");
+        String contrasenia = RegistrarUsuario.usuarios_contrasenias.get("rood92");
+        assertEquals("Hola_1234",contrasenia,"No se registran dos usuarios con el mismo nombre");
+
+    }
     //PODRIA IR UN CASO EN DONDE NO DEJE REGISTRARSE A UN USUARIO SI YA HAY UNO CON ESE NOMBRE DE USUARIO
 
 }
